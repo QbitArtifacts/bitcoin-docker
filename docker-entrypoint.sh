67b0c3ba2c175f20ac47a035092d691227f8858f
Lluis Santos <lluis@ngynx.com>
2019-02-28 17:54:38 +0100

    added network capabilities

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-bitcoind -server -testnet -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcuser=btc -rpcpassword=$RPCPASSWORD -prune=550
+if [[ "$NETWORK" == "testnet" ]];then
+    bitcoind -server -testnet -rpcbind=0.0.0.0 -rpcport=8332 -rpcallowip=0.0.0.0/0 -rpcuser=app -rpcpassword=$RPCPASSWORD -prune=550
+else
+    bitcoind -server -rpcbind=0.0.0.0 -rpcport=8332 -rpcallowip=0.0.0.0/0 -rpcuser=app -rpcpassword=$RPCPASSWORD -prune=550
+fi
 
